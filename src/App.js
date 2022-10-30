ddd3adbe060e35e4cc64545292145b05592dca4c
AliHaamedi <alihaamedin@gmail.com>
2022-10-30 12:23:25 +0330

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {BrowserRouter , Routes , Route} from "react-router-dom"
 import NavBar from './Components/Header/NavBar';
+import HeroSection from './Components/HeroSection/HeroSection';
 //import Cart from './Components/Cart/Cart';
 //import CoffeeList from './Components/List/CoffeeList';
 
 function App() {
   return (
-    <div className="container mx-auto flex">
+    <div className="container mx-auto">
       <NavBar />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element='' />
+          <Route path='/' element={<HeroSection />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/HeroSection/HeroSection.js
@@ -0,0 +1,19 @@
+import heroImg from "../../assets/img/coffeeHeero.jpg"
+
+
+function HeroSection() {
+    return ( 
+        <div className="flex p-4  flex-col md:flex-row">
+            <div className="md:w-1/2 mx-auto my-12 md:my-24">
+                <img src={heroImg} width="400px" alt="hero" />
+            </div>
+            <div className=" flex flex-col justify-evenly md:w-1/2 items-center my-8 md:my-24">
+                <h1 className="text-4xl font-yekan px-12">میدونستی میتونی میکس قهوتو خودت انتخاب کنی؟</h1>
+                <p className="text-xs px-12 self-start">با استی آپ از هر دانه ای که بخوای به هر اندازه ای که دوس دای قهوه سفارش بده</p>
+                <button className="bg-orange-300 rounded-full py-1 px-3 self-start mr-24">بزن بریم!</button>
+            </div>
+        </div>
+     );
+}
+
+export default HeroSection;
